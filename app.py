43e7ce68ee465706ed4c84c0fdbec980559b412b
yllhwa <yll2002mail@gmail.com>
2021-03-21 20:26:14 +0800

    开始处理前端

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, redirect, url_for,send_from_directory,request
 from config import Config
 from flask_mail import Message, Mail
 from api import auth
 import api.models
 from api.models import db
+from api.models import token_required
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+import functools
 
-app=Flask(__name__)
+app=Flask(__name__,static_url_path='')
 app.config.from_object(Config)
 
 mail_host = Mail()
@@ -18,9 +21,34 @@ def creatapp():
 db.create_all(app=creatapp())
 
 #creatapp()
+
+def login_required(view_func):
+    @functools.wraps(view_func)
+    def verify_login(*args, **kwargs):
+        try:
+            token = request.cookies.get('token')
+        except Exception as e:
+            print(e)
+            return redirect(url_for("login"))
+        
+        s = Serializer(app.config["SECRET_KEY"])
+        try:
+            s.loads(token)
+        except Exception:
+            return redirect(url_for("login"))
+        
+        return view_func(*args,**kwargs)
+    return verify_login
+
 @app.route('/')
+@app.route('/index')
+@login_required
 def index():
-    return "Hello, World!"
+    return send_from_directory("layout","index.html")
+
+@app.route('/login')
+def login():
+    return send_from_directory("layout","login.html")
 
 
 if __name__ == '__main__':
